d3eb5db5f983e4661867a2096f0804034f0229fd
francisco.mejias <francisco.mejias@mindbodyonline.com>
2021-06-22 22:19:58 -0400

    added cypress image snapshot plugin

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+import { addMatchImageSnapshotPlugin } from 'cypress-image-snapshot/command'
+
+addMatchImageSnapshotPlugin()
