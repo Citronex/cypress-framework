9dba9b1c41483dd8754f5657a58f7038d52348f9
francisco.mejias <francisco.mejias@mindbodyonline.com>
2021-06-22 22:46:18 -0400

    added helper functions in commands.js

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,5 +25,30 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 import { addMatchImageSnapshotPlugin } from 'cypress-image-snapshot/command'
+import { size } from 'cypress/types/lodash'
 
 addMatchImageSnapshotPlugin()
+
+Cypress.Commands.add('isVisible', selector => {
+    cy.get(selector).should('be.visible')
+})
+
+Cypress.Commands.add('isHidden', selector => {
+    cy.get(selector).should('not.exist')
+})
+
+Cypress.Commands.add('setResolution', size => {
+    if (Cypress._.isArray(size)) {
+        cy.viewport(size[0], size[1])
+    } else {
+        cy.viewport(size)
+    }
+})
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.get('#login_form').should('be.visible')
+    cy.get('#user_login').type(username)
+    cy.get('#user_password').type(password)
+    cy.get('#user_remember_me').click()
+    cy.contains('Sign in').click()
+})
