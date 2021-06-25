f0be900a05f0a9618f298ee38ecd2d033dcdab8d
francisco.mejias <francisco.mejias@mindbodyonline.com>
2021-06-24 22:15:44 -0400

    added login page to pages

---
+++ cypress/page-objects/pages/LoginPage.js
@@ -0,0 +1,14 @@
+export default class LoginPage extends BasePage {
+    static login(username, passowrd) {
+        cy.login(username, passowrd)
+    }
+
+    static clickForgotPasswordLink(){
+        cy.contains('Forgot your password ?').click()
+    }
+
+    static displayErrorMessage() {
+        //custom command 'isVisible' from commands.js
+        cy.isVisible('.alert.alert-error')
+    }
+}
