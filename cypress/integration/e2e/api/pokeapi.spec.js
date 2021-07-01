e1f868fd647d2c46b99225ebd9675f6584268f3c
francisco.mejias <francisco.mejias@mindbodyonline.com>
2021-06-30 21:59:19 -0400

    added validate status code test

--- cypress/integration/e2e/api/pokeapi.spec.js
+++ cypress/integration/e2e/api/pokeapi.spec.js
@@ -1,12 +1,21 @@
 /// <reference types="Cypress" />
+import { pokeapi_base_url } from '../../../../config'
 
 describe('Rest API Test with Cypress', () =>  {
     it('API Test - Validate Header', () => {
-        cy.request('https://pokeapi.co/api/v2/pokemon/25')
+        cy.request(`${pokeapi_base_url}v2/pokemon/25`)
         .as('pokemon')
         cy.get('@pokemon')
         .its('headers')
         .its('content-type')
         .should('include', 'application/json; charset=utf-8')
+    })
+
+    it('API Test - Validate Status Code', () => {
+        cy.request(`${pokeapi_base_url}v2/pokemon/25`)
+        .as('pokemon')
+        cy.get('@pokemon')
+        .its('status')
+        .should('equal', 200)
     })
 })
